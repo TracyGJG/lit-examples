10490493688c10d9a6212d12a5c14b7bc979dbe2
TGJ Gilmore <TracyGJG@users.noreply.github.com>
2024-11-30 16:34:48 +0000

    Use handelEvent

--- no-lit/my-button.js
+++ no-lit/my-button.js
@@ -1,85 +1,89 @@
 class Component extends HTMLElement {
-  static #style = `
-  .container {
-    padding: 8px;
-  }
-
-  button {
-    display: block;
-    overflow: hidden;
-    position: relative;
-    padding: 0 16px;
-    font-size: 16px;
-    font-weight: bold;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    cursor: pointer;
-    outline: none;
-
-    width: 100%;
-    height: 40px;
-
-    box-sizing: border-box;
-    border: 1px solid #a1a1a1;
-    background: #ffffff;
-    box-shadow: 0 2px 4px 0 rgba(0,0,0, 0.05), 
-      0 2px 8px 0 rgba(161,161,161, 0.4);
-    color: #363636;
-  }
-`;
-
-  static #template = `
-  <div class="container">
-    <button>Label</button>
-  </div>
-`;
-
-  static #domTemplate = document.createElement('template');
-
-  constructor() {
-    super();
-
-    Component.#domTemplate.innerHTML = `
-  <style>
-    ${Component.#style}
-  </style>
-
-  ${Component.#template}
-`;
-
-    this._shadowRoot = this.attachShadow({ mode: 'open' });
-    this._shadowRoot.appendChild(
-      Component.#domTemplate.content.cloneNode(true)
-    );
-
-    this.onClick = () => {};
-    this.$button = this._shadowRoot.querySelector('button');
-    this.$button.addEventListener('click', evt => this.onClick(evt));
-  }
-
-  get label() {
-    return this.getAttribute('label');
-  }
-
-  set label(value) {
-    this.setAttribute('label', value);
-  }
-
-  static get observedAttributes() {
-    return ['label'];
-  }
-
-  attributeChangedCallback(name, oldVal, newVal) {
-    console.log('attributeChangedCallback', name, oldVal, newVal);
-    if (oldVal !== newVal) {
-      this.render();
+	static style = `
+    .container {
+      padding: 8px;
     }
-  }
 
-  render() {
-    console.log('render', this.label);
-    this.$button.innerHTML = this.label;
-  }
+    button {
+      display: block;
+      overflow: hidden;
+      position: relative;
+      padding: 0 16px;
+      font-size: 16px;
+      font-weight: bold;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      cursor: pointer;
+      outline: none;
+
+      width: 100%;
+      height: 40px;
+
+      box-sizing: border-box;
+      border: 1px solid #a1a1a1;
+      background: #ffffff;
+      box-shadow: 0 2px 4px 0 rgba(0,0,0, 0.05), 
+        0 2px 8px 0 rgba(161,161,161, 0.4);
+      color: #363636;
+    }
+  `;
+
+	static template = `
+    <div class="container">
+      <button>Label</button>
+    </div>
+  `;
+
+	static domTemplate = document.createElement('template');
+
+	constructor() {
+		super();
+
+		Component.domTemplate.innerHTML = `
+      <style>
+        ${Component.style}
+      </style>
+
+      ${Component.template}
+    `;
+
+		this._shadowRoot = this.attachShadow({ mode: 'open' });
+		this._shadowRoot.appendChild(Component.domTemplate.content.cloneNode(true));
+
+		// this.onClick = () => {};
+		this.$button = this._shadowRoot.querySelector('button');
+		this.$button.addEventListener('click', this);
+	}
+
+	handleEvent(evt) {
+		if (evt.type === 'click' && this.onClick) {
+			this.onClick(evt);
+		}
+	}
+
+	get label() {
+		return this.getAttribute('label');
+	}
+
+	set label(value) {
+		this.setAttribute('label', value);
+	}
+
+	static get observedAttributes() {
+		return ['label'];
+	}
+
+	attributeChangedCallback(name, oldVal, newVal) {
+		console.log('attributeChangedCallback', name, oldVal, newVal);
+		if (oldVal !== newVal) {
+			this.render();
+		}
+	}
+
+	render() {
+		console.log('render', this.label);
+		this.$button.innerHTML = this.label;
+	}
 }
 
 window.customElements.define('my-button', Component);
